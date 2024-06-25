fcb2b7ba8e2736678140d40accbe9ff8dfca0317
lnl9904 <lenholoc132@gmail.com>
2024-06-25 18:01:47 +0700

    test

--- DAL/Form1.Designer.cs
+++ DAL/Form1.Designer.cs
@@ -29,22 +29,33 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(73, 104);
+            button1.Location = new Point(249, 110);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            button2.Location = new Point(148, 216);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
@@ -54,5 +65,6 @@
         #endregion
 
         private Button button1;
+        private Button button2;
     }
 }
